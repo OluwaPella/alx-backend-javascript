98af6e25b513150260e01744c8c1a30ee5ded4e9
oolaosebikan360@gmail.com <oolaosebikan350@gmail.com>
2024-05-08 15:59:00 +0100

    9-getFullBudget.js

--- 0x00-ES6_basic/9-getFullBudget.js
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 import getBudgetObject from './7-getBudgetObject.js';
 
 export default function getFullBudgetObject(income, gdp, capita) {
@@ -7,9 +8,10 @@ export default function getFullBudgetObject(income, gdp, capita) {
     getIncomeInDollars: (income) => {
       return `$${income}`;
     },
-    getIncomeInEuros:  (income) => {
+    getIncomeInEuros: (income) => {
       return `${income} euros`;
     },
   };
+  
   return fullBudget;
 }
