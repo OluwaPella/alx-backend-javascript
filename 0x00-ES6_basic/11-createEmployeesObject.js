bc3acd8d39973c0cb6c0d7a3705eea6c2ce7a47f
oolaosebikan360@gmail.com <oolaosebikan350@gmail.com>
2024-09-16 13:19:25 +0100

    add: indention error

--- 0x00-ES6_basic/11-createEmployeesObject.js
+++ 0x00-ES6_basic/11-createEmployeesObject.js
@@ -1,6 +1,5 @@
 export default function createEmployeesObject(departmentName, employees) {
-               return {
+      return {
           [departmentName]: employees
-        };
-   }
-   
+     };
+}
