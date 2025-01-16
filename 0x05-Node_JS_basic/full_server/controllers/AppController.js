fc81147b1d9a0c81670e231b44b7a7e83e20e578
oolaosebikan360@gmail.com <oolaosebikan350@gmail.com>
2025-01-16 01:34:48 +0100

    clear white space

--- 0x05-Node_JS_basic/full_server/controllers/AppController.js
+++ 0x05-Node_JS_basic/full_server/controllers/AppController.js
@@ -4,7 +4,7 @@
  */
 class AppController {
   static getHomepage(request, response) {
-    response.status(200).send('Hello Holberton School!'); 
+    response.status(200).send('Hello Holberton School!');
   }
 }
 export default AppController;
